36592d2e143d3400f46d3a43bbd4c0baad3130aa
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-10-08 10:42:44 -0500

    Add HTML content

---
+++ src/modules/addBook.js
@@ -0,0 +1,8 @@
+export default class Book {
+    // Book class: Represents a book
+    constructor(title, author, id) {
+      this.title = title;
+      this.author = author;
+      this.id = id;
+    }
+  }
